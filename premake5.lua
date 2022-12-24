ec13c9d1c1d3c60b965e02d7d995c937f632545c
Paul Lipczyk <panogrodek@yahoo.com>
2022-12-24 20:46:36 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,28 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	links{
+		"Dwmapi.lib"
+	}
+
 	files
 	{
+		"include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
+	
+	
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
@@ -26,32 +41,9 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
 	filter "system:windows"
 		systemversion "latest"
+
 
 		files
 		{
@@ -72,21 +64,11 @@ project "GLFW"
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
-
-		links
-		{
-			"Dwmapi.lib"
-		}
-
+		
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
-
+		
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
